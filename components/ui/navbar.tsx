e38ed5452f5c46aa5c1173e7b6820e216f40b51b
BengieVillesco <bvillesco@gmail.com>
2026-04-02 01:37:06 +0800

    feat(ui/navbar): add tooltip and icons

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import styles from "./navbar.module.scss";
+import { AlignVerticalJustifyEnd, Home, Send, UserRound } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./tooltip";
 
 export default function NavBar() {
   const [activeTab, setActiveTab] = useState<Tab>("home");
@@ -10,40 +12,72 @@ export default function NavBar() {
     <nav className={styles.navigation}>
       <ul>
         <li>
-          <a
-            href="#home"
-            className={activeTab === "home" ? styles.active : ""}
-            onClick={() => setActiveTab("home")}
-          >
-            Home
-          </a>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                href="#home"
+                className={activeTab === "home" ? styles.active : ""}
+                onClick={() => setActiveTab("home")}
+              >
+                <Home size={20} />
+                <span>Home</span>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent className="md:hidden">
+              <p>Home</p>
+            </TooltipContent>
+          </Tooltip>
         </li>
         <li>
-          <a
-            href="#about"
-            className={activeTab === "about" ? styles.active : ""}
-            onClick={() => setActiveTab("about")}
-          >
-            About
-          </a>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                href="#about"
+                className={activeTab === "about" ? styles.active : ""}
+                onClick={() => setActiveTab("about")}
+              >
+                <UserRound size={20} />
+                <span>About</span>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent className="md:hidden">
+              <p>About</p>
+            </TooltipContent>
+          </Tooltip>
         </li>
         <li>
-          <a
-            href="#project"
-            className={activeTab === "project" ? styles.active : ""}
-            onClick={() => setActiveTab("project")}
-          >
-            Project
-          </a>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                href="#project"
+                className={activeTab === "project" ? styles.active : ""}
+                onClick={() => setActiveTab("project")}
+              >
+                <AlignVerticalJustifyEnd size={20} />
+                <span>Project</span>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent className="md:hidden">
+              <p>Project</p>
+            </TooltipContent>
+          </Tooltip>
         </li>
         <li>
-          <a
-            href="#contact"
-            className={activeTab === "contact" ? styles.active : ""}
-            onClick={() => setActiveTab("contact")}
-          >
-            Contact
-          </a>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                href="#contact"
+                className={activeTab === "contact" ? styles.active : ""}
+                onClick={() => setActiveTab("contact")}
+              >
+                <Send size={20} />
+                <span>Contact</span>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent className="md:hidden">
+              <p>Contact</p>
+            </TooltipContent>
+          </Tooltip>
         </li>
       </ul>
     </nav>

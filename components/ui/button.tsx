eabdbbf0dce37729a4ed89b46b14e28cfee33591
BengieVillesco <bvillesco@gmail.com>
2026-04-03 02:51:31 +0800

    feat(ui/button): add size and height props with improved class handling

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,14 +1,19 @@
+import { cn } from "@/lib/utils";
 import styles from "./button.module.scss";
 
 type ButtonProps = {
   children: React.ReactNode;
   variant?: "default" | "outline" | "ghost";
+  size?: "small" | "medium" | "large";
+  height?: "default" | "tall";
   className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button({
   children,
   variant = "default",
+  size = "medium",
+  height = "tall",
   className = "",
   ...props
 }: ButtonProps) {
@@ -20,8 +25,26 @@ export default function Button({
     ghost: `${styles.ghost}`,
   };
 
+  const sizes = {
+    small: {
+      default: styles.smDefault,
+      tall: styles.smTall,
+    },
+    medium: {
+      default: styles.mdDefault,
+      tall: styles.mdTall,
+    },
+    large: {
+      default: styles.lgDefault,
+      tall: styles.lgTall,
+    },
+  };
+
   return (
-    <button className={`${base} ${variants[variant]} ${className}`} {...props}>
+    <button
+      className={cn(base, variants[variant], sizes[size][height], className)}
+      {...props}
+    >
       {children}
     </button>
   );

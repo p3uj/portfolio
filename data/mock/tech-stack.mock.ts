8c04799aec019ab0f1618d3d6b942406c5823913
BengieVillesco <bvillesco@gmail.com>
2026-04-03 02:49:46 +0800

    chore(data/mock): add mock data file for tech stack

---
+++ data/mock/tech-stack.mock.ts
@@ -0,0 +1,89 @@
+import { TechStack } from "@/types/tech-stack";
+
+export const TECH_STACK_MOCK: TechStack[] = [
+  {
+    id: 1,
+    name: "django",
+    type: "backend",
+  },
+  {
+    id: 2,
+    name: "react",
+    type: "frontend",
+  },
+  {
+    id: 3,
+    name: "laravel",
+    type: "backend",
+  },
+  {
+    id: 4,
+    name: "html5",
+    type: "frontend",
+  },
+  {
+    id: 5,
+    name: "css3",
+    type: "frontend",
+  },
+  {
+    id: 6,
+    name: "sass",
+    type: "frontend",
+  },
+  {
+    id: 7,
+    name: "js",
+    type: "frontend",
+  },
+  {
+    id: 8,
+    name: "typescript",
+    type: "frontend",
+  },
+  {
+    id: 9,
+    name: "tailwindcss",
+    type: "frontend",
+  },
+  {
+    id: 10,
+    name: "nextjs",
+    type: "frontend",
+  },
+  {
+    id: 11,
+    name: "mysql",
+    type: "database",
+  },
+  {
+    id: 12,
+    name: "postgresql",
+    type: "database",
+  },
+  {
+    id: 13,
+    name: "firebase",
+    type: "backend",
+  },
+  {
+    id: 14,
+    name: "flutterflow",
+    type: "tool",
+  },
+  {
+    id: 15,
+    name: "vitejs",
+    type: "tool",
+  },
+  {
+    id: 16,
+    name: "git",
+    type: "tool",
+  },
+  {
+    id: 17,
+    name: "github",
+    type: "tool",
+  },
+];

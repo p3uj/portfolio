1b9c023ffc27509f0de96b061cd2dae0c5dc7b98
BengieVillesco <bvillesco@gmail.com>
2026-04-03 02:58:21 +0800

    feat(home/card): add intro card

---
+++ app/home/card/intro-card.tsx
@@ -0,0 +1,27 @@
+import { Card } from "@/components/ui/card";
+import styles from "./intro-card.module.scss";
+import Button from "@/components/ui/button";
+import { MousePointerClick } from "lucide-react";
+
+export default function IntroCard() {
+  return (
+    <Card className={styles.intro}>
+      <h1>Bengie Villesco</h1>
+      <h2>Frontend Developer</h2>
+
+      {/* prettier-ignore */}
+      <p>
+        Bengie is a fourth-year IT student at the Polytechnic University of the Philippines – Quezon City, passionate about full-stack development, UI/UX, cybersecurity, and data analysis. He enjoys building projects and creating meaningful tech solutions.
+      </p>
+
+      <section>
+        <Button>
+          <MousePointerClick className="h-[18px] w-fit" />
+          Hire Me
+        </Button>
+        <Button variant="outline">View Projects</Button>
+        <Button variant="outline">View Resume</Button>
+      </section>
+    </Card>
+  );
+}

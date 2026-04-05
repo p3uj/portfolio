fceef8f1744afb13724d187c8bc30d32d0e763c7
BengieVillesco <bvillesco@gmail.com>
2026-04-05 22:38:56 +0800

    feat(project): correct section title and display cards

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,9 +1,61 @@
+import { Card } from "@/components/ui/card/card";
 import styles from "./page.module.scss";
 
 export default function Project() {
   return (
     <section id="project" className={styles.project}>
-      <h1>Project Section</h1>
+      <h1>My Projects</h1>
+
+      <section>
+        <Card>
+          <img
+            src="/images/projects/project1/Image1.png"
+            alt="Image project 1"
+          />
+        </Card>
+
+        <Card>
+          <img
+            src="/images/projects/project1/Image2.png"
+            alt="Image project 1"
+          />
+        </Card>
+
+        <Card>
+          <img
+            src="/images/projects/project1/Image3.png"
+            alt="Image project 1"
+          />
+        </Card>
+
+        <Card>
+          <img
+            src="/images/projects/project1/Image1.png"
+            alt="Image project 1"
+          />
+        </Card>
+
+        <Card>
+          <img
+            src="/images/projects/project1/Image2.png"
+            alt="Image project 1"
+          />
+        </Card>
+
+        <Card>
+          <img
+            src="/images/projects/project1/Image3.png"
+            alt="Image project 1"
+          />
+        </Card>
+
+        <Card>
+          <img
+            src="/images/projects/project1/Image1.png"
+            alt="Image project 1"
+          />
+        </Card>
+      </section>
     </section>
   );
 }

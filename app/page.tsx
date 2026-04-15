ecdce38b8f1c1c404e37ca43f4f71f7fffccb66d
BengieVillesco <bvillesco@gmail.com>
2026-04-16 00:05:01 +0800

    feat(page): add modal state management and project selection handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import NavBar from "@/components/ui/navbar/navbar";
 import styles from "./page.module.scss";
 import Home from "./home/page";
@@ -5,18 +7,33 @@ import About from "./about/page";
 import Project from "./project/page";
 import Contact from "./contact/page";
 import Footer from "@/components/ui/footer/footer";
+import Modal from "@/components/ui/modal/modal";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [projectId, setProjectId] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (!modalOpen) {
+      setProjectId(null);
+    }
+  }, [modalOpen]);
+
   return (
-    <div className={styles.layout}>
-      <NavBar />
-      <main>
-        <Home />
-        <About />
-        <Project />
-        <Contact />
-        <Footer />
-      </main>
-    </div>
+    <>
+      {modalOpen && <Modal open={setModalOpen} project_id={projectId} />}
+
+      <div className={styles.layout}>
+        <NavBar />
+        <main>
+          <Home />
+          <About />
+          <Project modalOpen={setModalOpen} id={setProjectId} />
+          <Contact />
+          <Footer />
+        </main>
+      </div>
+    </>
   );
 }

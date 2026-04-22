b3b100699132d0716623120be8b84b2c880b471b
BengieVillesco <bvillesco@gmail.com>
2026-04-23 01:21:41 +0800

    feat(utils/project): enhance project helper functions to support feature data and flexible image retrieval
    - update GetImage to handle both cover images (by project_id) and feature images (by image id)
    - add GetFeature helper to fetch project features and attach corresponding image paths
    - extend GetProject to include feature data in returned project object

--- lib/utils/project.ts
+++ lib/utils/project.ts
@@ -1,16 +1,31 @@
-import { PROJECT_IMAGES_MOCK, PROJECT_MOCK } from "@/data/mock/project-mock";
+import {
+  PROJECT_FEATURE_MOCK,
+  PROJECT_IMAGES_MOCK,
+  PROJECT_MOCK,
+} from "@/data/mock/project-mock";
 import { TECH_STACK_MOCK } from "@/data/mock/tech-stack.mock";
 
-function GetImage(project_id: number, is_cover: boolean) {
-  return PROJECT_IMAGES_MOCK.find(
-    (image) => image.project_id === project_id && image.is_cover === is_cover,
-  )?.image_path;
+function GetImage(
+  id: number | null = null,
+  project_id: number | null = null,
+  is_cover: boolean = false,
+) {
+  if (is_cover === true) {
+    // Overview Image
+    return PROJECT_IMAGES_MOCK.find(
+      (image) => image.project_id === project_id && image.is_cover === is_cover,
+    );
+  } else {
+    // Feature Image
+    return PROJECT_IMAGES_MOCK.find((image) => image.id === id);
+  }
 }
 
 function GetProject(project_id: number) {
   const project = PROJECT_MOCK.find((project) => project.id === project_id);
-  const image = GetImage(project_id, true);
+  const image = GetImage(null, project_id, true)?.image_path;
   const tech_stack = GetTechStack(project?.tech_stack_id);
+  const feature = GetFeature(project_id);
 
   if (!project) return null;
 
@@ -20,11 +35,23 @@ function GetProject(project_id: number) {
     ...cleanedProject,
     cover: image,
     tech_stack,
+    feature,
   };
 }
 
 function GetTechStack(tech_stack_id: number[] = []) {
   return TECH_STACK_MOCK.filter((tech) => tech_stack_id.includes(tech.id));
+}
+
+function GetFeature(project_id: number) {
+  const features = PROJECT_FEATURE_MOCK.filter(
+    (feature) => feature.project_id === project_id,
+  ).map((feature) => ({
+    ...feature,
+    image_path: GetImage(feature.image_id)?.image_path,
+  }));
+
+  return features;
 }
 
 export { GetImage, GetProject };

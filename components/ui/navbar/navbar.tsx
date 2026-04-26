a6ef1d1e3a7837995fe5d09aadab3fda41b86456
BengieVillesco <bvillesco@gmail.com>
2026-04-26 23:30:35 +0800

    feat(navbar): remove tooltip

--- components/ui/navbar/navbar.tsx
+++ components/ui/navbar/navbar.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import styles from "./navbar.module.scss";
 import { AlignVerticalJustifyEnd, Home, Send, UserRound } from "lucide-react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "../tooltip/tooltip";
 import { NavBarTab } from "@/types";
 import { useRouter } from "next/navigation";
 import Button from "../button/button";
@@ -51,76 +50,48 @@ export default function NavBar() {
     <nav className={styles.navigation}>
       <ul>
         <li>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                className={cn(activeTab === "home" ? styles.active : "")}
-                onClick={() => {
-                  setActiveTab("home");
-                  router.push("/#home");
-                }}>
-                <Home size={20} />
-                <span>Home</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent className="md:hidden">
-              <p>Home</p>
-            </TooltipContent>
-          </Tooltip>
+          <Button
+            className={cn(activeTab === "home" ? styles.active : "")}
+            onClick={() => {
+              setActiveTab("home");
+              router.push("/#home");
+            }}>
+            <Home size={20} />
+            <span>Home</span>
+          </Button>
         </li>
         <li>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                className={cn(activeTab === "about" && styles.active)}
-                onClick={() => {
-                  setActiveTab("about");
-                  router.push("/#about");
-                }}>
-                <UserRound size={20} />
-                <span>About</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent className="md:hidden">
-              <p>About</p>
-            </TooltipContent>
-          </Tooltip>
+          <Button
+            className={cn(activeTab === "about" && styles.active)}
+            onClick={() => {
+              setActiveTab("about");
+              router.push("/#about");
+            }}>
+            <UserRound size={20} />
+            <span>About</span>
+          </Button>
         </li>
         <li>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                className={cn(activeTab === "project" && styles.active)}
-                onClick={() => {
-                  setActiveTab("project");
-                  router.push("/#project");
-                }}>
-                <AlignVerticalJustifyEnd size={20} />
-                <span>Project</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent className="md:hidden">
-              <p>Project</p>
-            </TooltipContent>
-          </Tooltip>
+          <Button
+            className={cn(activeTab === "project" && styles.active)}
+            onClick={() => {
+              setActiveTab("project");
+              router.push("/#project");
+            }}>
+            <AlignVerticalJustifyEnd size={20} />
+            <span>Project</span>
+          </Button>
         </li>
         <li>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button
-                className={cn(activeTab === "contact" && styles.active)}
-                onClick={() => {
-                  setActiveTab("contact");
-                  router.push("/#contact");
-                }}>
-                <Send size={20} />
-                <span>Contact</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent className="md:hidden">
-              <p>Contact</p>
-            </TooltipContent>
-          </Tooltip>
+          <Button
+            className={cn(activeTab === "contact" && styles.active)}
+            onClick={() => {
+              setActiveTab("contact");
+              router.push("/#contact");
+            }}>
+            <Send size={20} />
+            <span>Contact</span>
+          </Button>
         </li>
       </ul>
     </nav>

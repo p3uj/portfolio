09e9a7c69e4d3233bab88a55d4ac5217e6ac094c
BengieVillesco <bvillesco@gmail.com>
2026-05-08 00:49:03 +0800

    feat: add initial tech-stack section

---
+++ app/tech-stack/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.scss";
+
+export function TechStack() {
+  return (
+    <section id="tech-stack" className={styles.techStack}>
+      <h1>Tech Stack Section</h1>
+    </section>
+  );
+}

02607e55c10b84b70303e49b4a82e84cf8d9b6a5
BengieVillesco <bvillesco@gmail.com>
2026-05-08 00:38:14 +0800

    feat(mock): add navbar-mock data

---
+++ data/mock/navbar-mock.ts
@@ -0,0 +1,29 @@
+import { NavBar } from "@/types";
+
+export const NAV_BAR: NavBar[] = [
+  {
+    id: 1,
+    name: "home",
+    section: "home",
+  },
+  {
+    id: 2,
+    name: "about",
+    section: "about",
+  },
+  {
+    id: 3,
+    name: "project",
+    section: "project",
+  },
+  {
+    id: 4,
+    name: "tech stack",
+    section: "tech-stack",
+  },
+  {
+    id: 5,
+    name: "contact",
+    section: "contact",
+  },
+];

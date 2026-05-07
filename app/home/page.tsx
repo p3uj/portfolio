e080217bc70ecd03d343b0158db8f6a92dbe86eb
BengieVillesco <bvillesco@gmail.com>
2026-05-07 20:21:03 +0800

    feat(home): update and re-design home section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,17 @@
 import IntroCard from "./card/intro-card";
 import styles from "./page.module.scss";
+import { Profile } from "./profile/profile";
 import TechStack from "./tech-stack/tech-stack";
 
 export default function Home() {
   return (
     <section id="home" className={styles.home}>
-      <TechStack />
-      <TechStack direction="left-to-right" />
+      {/* <TechStack />
+      <TechStack direction="left-to-right" /> */}
+      <Profile />
       <IntroCard />
-      <TechStack />
-      <TechStack direction="left-to-right" />
+      {/* <TechStack />
+      <TechStack direction="left-to-right" /> */}
     </section>
   );
 }

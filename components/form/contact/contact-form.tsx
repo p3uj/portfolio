ac530a948b67f51d07c88e57ef171626e809a38f
BengieVillesco <bvillesco@gmail.com>
2026-05-08 14:24:11 +0800

    feat(form/contact-form): integrate React Hook Form with Zod validation

--- components/form/contact/contact-form.tsx
+++ components/form/contact/contact-form.tsx
@@ -2,41 +2,81 @@ import { Card } from "@/components/ui/card/card";
 import styles from "./contact-form.module.scss";
 import { LocateIcon, Mail, Phone, Send } from "lucide-react";
 import Button from "@/components/ui/button/button";
+import { useForm } from "react-hook-form";
+import { contactSchema } from "@/lib/schema/contact";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<z.infer<typeof contactSchema>>({
+    resolver: zodResolver(contactSchema),
+    mode: "onChange",
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  const submission = async (values: z.infer<typeof contactSchema>) => {
+    console.table(values);
+  };
+
   return (
     <>
       <Card className={styles.contactForm}>
-        <form action="" method="post">
+        <form onSubmit={handleSubmit(submission)}>
           {/* Name */}
           <fieldset>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name" data-error={!!errors.name}>
+              Name
+            </label>
             <input
+              data-error={!!errors.name}
               type="text"
-              name="name"
               id="name"
               placeholder="e.g Bengie Villesco"
+              {...register("name")}
             />
+
+            {errors.name && <span>{errors.name.message}</span>}
           </fieldset>
 
           {/* Email */}
           <fieldset>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" data-error={!!errors.email}>
+              Email
+            </label>
             <input
+              data-error={!!errors.email}
               type="text"
-              name="email"
               id="email"
               placeholder="e.g bvillesco@gmail.com"
+              {...register("email")}
             />
+
+            {errors.email && <span>{errors.email.message}</span>}
           </fieldset>
 
           {/* Message */}
           <fieldset>
-            <label htmlFor="message">Message</label>
-            <textarea id="message" placeholder="enter your message"></textarea>
+            <label htmlFor="message" data-error={!!errors.message}>
+              Message
+            </label>
+            <textarea
+              data-error={!!errors.message}
+              id="message"
+              placeholder="enter your message"
+              {...register("message")}></textarea>
+
+            {errors.message && <span>{errors.message.message}</span>}
           </fieldset>
 
-          <Button type="submit">
+          <Button type="submit" disabled={!isValid}>
             <Send size={20} />
             Send
           </Button>

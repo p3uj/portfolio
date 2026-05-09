e9d70f852099842c82b0967cd2ad3e601eeeac2e
BengieVillesco <bvillesco@gmail.com>
2026-05-09 18:00:22 +0800

    feat(home): add buttons and status as badge and redesign badge roles

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,39 +6,50 @@ import TechStack from "./tech-stack/tech-stack";
 import { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card/card";
 import Badge from "@/components/ui/badge/badge";
+import Button from "@/components/ui/button/button";
+import { MousePointerClick } from "lucide-react";
 
 export default function Home() {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % ROLES.length);
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <section id="home" className={styles.home}>
+      <Badge name="available for work" className={styles.status} />
+
       <span>
         Hi, I'm
         <h1>Bengie</h1>
       </span>
 
-      <Card>
+      <Card className={styles.card1}>
         {/* prettier-ignore */}
         <p>
         I create complete digital experiences by combining UI/UX design with full stack development. I focus on building fast, responsive, and user-friendly applications that solve real problems.
       </p>
       </Card>
 
-      <Card>
+      <Card className={styles.card2}>
         <p>Let's build something meaningful together.</p>
       </Card>
 
-      <Badge name={ROLES[index]} className={styles.role} />
+      <section className={styles.buttonWrapper}>
+        <Button>
+          <MousePointerClick className="h-[18px] w-fit" />
+          Hire Me
+        </Button>
+        <Button variant="outline"> Projects</Button>
+        <Button variant="outline">Resume</Button>
+      </section>
 
       <section className={styles.profileWrapper}>
+        <section className={styles.roleWrapper}>
+          {ROLES.map((role, index) => (
+            <Badge
+              key={index}
+              name={role}
+              className={styles[`role-${index}`]}
+            />
+          ))}
+        </section>
+
         <img src="./images/ID.png" alt="Bengie Villesco" />
       </section>
       {/* <TechStack />

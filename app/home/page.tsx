8ce6417611ebd215180b41ebb5c92f3ea7d85e17
BengieVillesco <bvillesco@gmail.com>
2026-05-10 21:55:18 +0800

    chore(home): remove unused imports and comments

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,5 @@
 import { ROLES } from "@/types";
-import IntroCard from "./card/intro-card";
 import styles from "./page.module.scss";
-import { Profile } from "./profile/profile";
-import TechStack from "./tech-stack/tech-stack";
-import { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card/card";
 import Badge from "@/components/ui/badge/badge";
 import Button from "@/components/ui/button/button";
@@ -82,12 +78,6 @@ export default function Home() {
 
         <img src="./images/ID.png" alt="Bengie Villesco" />
       </section>
-      {/* <TechStack />
-      <TechStack direction="left-to-right" /> */}
-      {/* <Profile /> */}
-      {/* <IntroCard /> */}
-      {/* <TechStack />
-      <TechStack direction="left-to-right" /> */}
     </section>
   );
 }

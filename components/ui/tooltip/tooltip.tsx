f94509327563e8f50f126caa4f7365560e1b50b8
BengieVillesco <bvillesco@gmail.com>
2026-05-12 16:17:01 +0800

    feat(ui/tooltip): extend content props to include hideArrow

--- components/ui/tooltip/tooltip.tsx
+++ components/ui/tooltip/tooltip.tsx
@@ -33,9 +33,12 @@ function TooltipTrigger({
 function TooltipContent({
   className,
   sideOffset = 0,
+  hideArrow = false,
   children,
   ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Content>) {
+}: React.ComponentProps<typeof TooltipPrimitive.Content> & {
+  hideArrow?: boolean;
+}) {
   return (
     <TooltipPrimitive.Portal>
       <TooltipPrimitive.Content
@@ -45,10 +48,12 @@ function TooltipContent({
           "z-[99999] inline-flex w-fit max-w-xs origin-(--radix-tooltip-content-transform-origin) items-center gap-1.5 rounded-md bg-neutral-50 dark:bg-neutral-900 px-3 py-1.5 text-xs text-neutral-900 dark:text-neutral-50 has-data-[slot=kbd]:pr-1.5 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 **:data-[slot=kbd]:relative **:data-[slot=kbd]:isolate **:data-[slot=kbd]:z-50 **:data-[slot=kbd]:rounded-sm data-[state=delayed-open]:animate-in data-[state=delayed-open]:fade-in-0 data-[state=delayed-open]:zoom-in-95 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
           className,
         )}
-        {...props}
-      >
+        {...props}>
         {children}
-        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-neutral-50 dark:bg-neutral-900 fill-neutral-50 dark:fill-neutral-900" />
+
+        {!hideArrow && (
+          <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-neutral-50 dark:bg-neutral-900 fill-neutral-50 dark:fill-neutral-900" />
+        )}
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   );

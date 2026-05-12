57aa875faeda822bd4d8b48f92d6b97d8cb3f19b
BengieVillesco <bvillesco@gmail.com>
2026-05-12 16:21:30 +0800

    feat(sections/contact): add copy icon with tooltip and implement its functionality

--- components/sections/contact/contack-section.tsx
+++ components/sections/contact/contack-section.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import { Card } from "@/components/ui/card/card";
 import styles from "./contact-section.module.scss";
-import { LocateIcon, Mail, Phone, Send } from "lucide-react";
+import { Copy, LocateIcon, Mail, Phone, Send } from "lucide-react";
 import ContactForm from "@/components/form/contact/contact-form";
+import { useEffect, useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip/tooltip";
 
 export default function ContactSection() {
+  const [copiedIndex, setCopiedIndex] = useState<number | null>(null);
+
+  const handleCopy = (text: string, index: number) => {
+    navigator.clipboard.writeText(text);
+
+    setCopiedIndex(index);
+
+    setTimeout(() => {
+      setCopiedIndex(null);
+    }, 1000);
+  };
+
   return (
     <section id="contact" className={styles.contact}>
       {/* <h2>Contact</h2> */}
@@ -16,33 +36,63 @@ export default function ContactSection() {
       </p>
 
       {/* Email */}
-      <Card>
+      <Card className={styles.card}>
         <Mail />
 
         <fieldset>
           <label>Email</label>
           <p>bvillesco@gmail.com</p>
         </fieldset>
+
+        <Tooltip open={copiedIndex === 1}>
+          <TooltipTrigger asChild>
+            <Copy onClick={() => handleCopy("bvillesco@gmail.com", 1)} />
+          </TooltipTrigger>
+
+          <TooltipContent hideArrow className="mb-2">
+            <p>Copied</p>
+          </TooltipContent>
+        </Tooltip>
       </Card>
 
       {/* Phone Number */}
-      <Card>
+      <Card className={styles.card}>
         <Phone />
 
         <fieldset>
           <label>Phone Number</label>
           <p>0938874883</p>
         </fieldset>
+
+        <Tooltip open={copiedIndex === 2}>
+          <TooltipTrigger asChild>
+            <Copy onClick={() => handleCopy("0938874883", 2)} />
+          </TooltipTrigger>
+
+          <TooltipContent hideArrow className="mb-2">
+            <p>Copied</p>
+          </TooltipContent>
+        </Tooltip>
       </Card>
 
       {/* Location */}
-      <Card>
+      <Card className={styles.card}>
         <LocateIcon />
 
         <fieldset>
           <label>Location</label>
           <p>Quezon City, Philippines</p>
         </fieldset>
+
+        <Tooltip open={copiedIndex === 3}>
+          <TooltipTrigger asChild>
+            <Copy onClick={() => handleCopy("Quezon City, Philippines", 3)} />
+          </TooltipTrigger>
+
+          <TooltipContent hideArrow className="mb-2">
+            <p>Copied</p>
+          </TooltipContent>
+        </Tooltip>
       </Card>
 
       <ContactForm />

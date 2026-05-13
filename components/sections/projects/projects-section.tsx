ce64b637d7ff4003340a3b3ba39707b0d74e907a
BengieVillesco <bvillesco@gmail.com>
2026-05-13 16:33:26 +0800

    feat(sections/project): implement tab filtering with functionality

--- components/sections/projects/projects-section.tsx
+++ components/sections/projects/projects-section.tsx
@@ -4,7 +4,9 @@ import { Card } from "@/components/ui/card/card";
 import styles from "./projects-section.module.scss";
 import Button from "@/components/ui/button/button";
 import { PROJECT_MOCK } from "@/data/mock/project-mock";
-import { GetImage } from "@/lib/utils/project";
+import { CountProject, GetImage } from "@/lib/utils/project";
+import { useEffect, useState } from "react";
+import { PROJECT_TAB, ProjectTab } from "@/types";
 
 interface ProjectProps {
   modalOpen: (value: boolean) => void;
@@ -12,12 +14,41 @@ interface ProjectProps {
 }
 
 export default function ProjectsSection({ modalOpen, id }: ProjectProps) {
+  const [activeTab, setActiveTab] = useState<ProjectTab>("all");
+  const [data, setData] = useState(PROJECT_MOCK);
+
+  const getData = (tab: ProjectTab) => {
+    if (tab != "all") {
+      return PROJECT_MOCK.filter((project) => project.type === tab);
+    }
+
+    return PROJECT_MOCK;
+  };
+
+  useEffect(() => {
+    const data = getData(activeTab);
+
+    setData(data);
+  }, [activeTab]);
+
   return (
     <section id="project" className={styles.project}>
       <h1>Featured Projects</h1>
 
-      <section>
-        {PROJECT_MOCK.map((project) => (
+      <section className={styles.tab}>
+        {PROJECT_TAB.map((tab, index) => (
+          <Button
+            key={index}
+            variant="ghost"
+            className={activeTab === tab ? styles.active : ""}
+            onClick={() => setActiveTab(tab)}>
+            {tab} <span>({CountProject(tab)})</span>
+          </Button>
+        ))}
+      </section>
+
+      <section className={styles.content}>
+        {data.map((project) => (
           <Card
             onClick={() => {
               modalOpen(true);

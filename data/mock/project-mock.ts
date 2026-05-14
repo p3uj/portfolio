85a23d0cf775012fb998d3bbf3831abf0b1e6112
BengieVillesco <bvillesco@gmail.com>
2026-05-14 14:00:36 +0800

    refactor(mock/project): update project mocks with real portfolio content

--- data/mock/project-mock.ts
+++ data/mock/project-mock.ts
@@ -3,43 +3,12 @@ import { Project, ProjectFeature, ProjectImage } from "@/types";
 export const PROJECT_MOCK: Project[] = [
   {
     id: 1,
-    skill_id: [1, 2, 3, 4, 5, 6],
-    name: "Project 1",
+    skill_id: [2, 4, 5, 1, 19, 22, 25],
+    name: "BizTrend Forecast",
     description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam dolore, doloribus facilis cumque nostrum rerum rem at nesciunt nisi dolorem fuga sit accusantium ad quidem, aut impedit temporibus debitis quod!",
+      "BizTrend Forecast is a predictive system for the business industry that utilizing data analytics and machine learning to forecast trends such as growth rate, revenue, and least crowded.",
     type: "web",
-  },
-  {
-    id: 2,
-    skill_id: [1, 2, 3, 4, 5, 6],
-    name: "Project 2",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam dolore, doloribus facilis cumque nostrum rerum rem at nesciunt nisi dolorem fuga sit accusantium ad quidem, aut impedit temporibus debitis quod!",
-    type: "web",
-  },
-  {
-    id: 3,
-    skill_id: [1, 2, 3, 4, 5, 6],
-    name: "Project 3",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam dolore, doloribus facilis cumque nostrum rerum rem at nesciunt nisi dolorem fuga sit accusantium ad quidem, aut impedit temporibus debitis quod!",
-    type: "web",
-  },
-  {
-    id: 4,
-    skill_id: [1, 2, 3, 4, 5, 6],
-    name: "Project 4",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam dolore, doloribus facilis cumque nostrum rerum rem at nesciunt nisi dolorem fuga sit accusantium ad quidem, aut impedit temporibus debitis quod!",
-    type: "mobile",
-  },
-  {
-    id: 5,
-    skill_id: [1, 2, 3, 4, 5, 6],
-    name: "Project 5",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam dolore, doloribus facilis cumque nostrum rerum rem at nesciunt nisi dolorem fuga sit accusantium ad quidem, aut impedit temporibus debitis quod!",
-    type: "UI/UX",
+    watch_demo_link: "https://youtu.be/3O3qhtrHxSg?si=755AjOTBhFdhQmIe",
   },
 ];
 
@@ -48,49 +17,41 @@ export const PROJECT_FEATURE_MOCK: ProjectFeature[] = [
     id: 1,
     project_id: 1,
     image_id: 2,
-    name: "Feature 1",
+    name: "Secure Login Authentication",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus eaque autem non nam aut ducimus tempore mollitia dolorum culpa, magnam earum explicabo ullam repudiandae rerum debitis adipisci sapiente nesciunt?",
+      "The system uses JSON Web Token (JWT)-based authentication to securely verify user identities and protect access to authorized features. It ensures secure login sessions, role-based access control, and enhanced data protection by validating user credentials and managing authenticated access efficiently.",
   },
   {
     id: 2,
     project_id: 1,
     image_id: 3,
-    name: "Feature 2",
+    name: "Industry Trend Monitoring Dashboard",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora libero esse id itaque officiis repellat suscipit. Architecto, possimus ullam magnam illum velit autem beatae repudiandae, voluptatum, quo quasi incidunt labore?",
+      "The system displays the top 5 predicted industry trends generated through machine learning forecasts for short-term (1 year), mid-term (3 years), and long-term (5 years) periods. It also supports filtering by growing, revenue-generating, and least crowded industry sectors for easier trend analysis.",
   },
   {
     id: 3,
-    project_id: 2,
-    image_id: 5,
-    name: "Feature 1",
+    project_id: 1,
+    image_id: 4,
+    name: "Dataset Upload and Machine Learning-Based Prediction",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus eaque autem non nam aut ducimus tempore mollitia dolorum culpa, magnam earum explicabo ullam repudiandae rerum debitis adipisci sapiente nesciunt?",
+      "The system allows users to upload datasets with built-in validation to ensure the file meets the required column names and data types. Once validated and accepted, the dataset is processed by the machine learning model to generate prediction results.",
   },
   {
     id: 4,
-    project_id: 2,
-    image_id: 6,
-    name: "Feature 2",
+    project_id: 1,
+    image_id: 5,
+    name: "User Registration and Management",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora libero esse id itaque officiis repellat suscipit. Architecto, possimus ullam magnam illum velit autem beatae repudiandae, voluptatum, quo quasi incidunt labore?",
+      "The system enables administrators to register new users and manage account status by activating or deactivating users. The system also supports filtering to view all, active, and inactive users for easier account management.",
   },
   {
     id: 5,
-    project_id: 3,
-    image_id: 8,
-    name: "Feature 1",
+    project_id: 1,
+    image_id: 6,
+    name: "Account Profile Management",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus eaque autem non nam aut ducimus tempore mollitia dolorum culpa, magnam earum explicabo ullam repudiandae rerum debitis adipisci sapiente nesciunt?",
-  },
-  {
-    id: 6,
-    project_id: 3,
-    image_id: 9,
-    name: "Feature 2",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora libero esse id itaque officiis repellat suscipit. Architecto, possimus ullam magnam illum velit autem beatae repudiandae, voluptatum, quo quasi incidunt labore?",
+      "The system allows users to update their personal information, including their first and last name, and change their profile picture for personalized account management.",
   },
 ];
 
@@ -98,67 +59,43 @@ export const PROJECT_IMAGES_MOCK: ProjectImage[] = [
   {
     id: 1,
     project_id: 1,
-    image_path: "/images/projects/project1/Image1.png",
+    image_path:
+      "/images/projects/biztrend-forecast/biztrend-forecast-landing-page.svg",
     is_cover: true,
   },
   {
     id: 2,
     project_id: 1,
-    image_path: "/images/projects/project1/Image2.png",
+    image_path:
+      "/images/projects/biztrend-forecast/biztrend-forecast-login.svg",
     is_cover: false,
   },
   {
     id: 3,
     project_id: 1,
-    image_path: "/images/projects/project1/Image3.png",
+    image_path:
+      "/images/projects/biztrend-forecast/biztrend-forecast-trend-monitoring.svg",
     is_cover: false,
   },
   {
     id: 4,
-    project_id: 2,
-    image_path: "/images/projects/project1/Image2.png",
+    project_id: 1,
+    image_path:
+      "/images/projects/biztrend-forecast/biztrend-forecast-prediction.svg",
     is_cover: true,
   },
   {
     id: 5,
-    project_id: 2,
-    image_path: "/images/projects/project1/Image1.png",
+    project_id: 1,
+    image_path:
+      "/images/projects/biztrend-forecast/biztrend-forecast-user-management.svg",
     is_cover: false,
   },
   {
     id: 6,
-    project_id: 3,
-    image_path: "/images/projects/project1/Image3.png",
+    project_id: 1,
+    image_path:
+      "/images/projects/biztrend-forecast/biztrend-forecast-profile-management.svg",
     is_cover: false,
-  },
-  {
-    id: 7,
-    project_id: 3,
-    image_path: "/images/projects/project1/Image3.png",
-    is_cover: true,
-  },
-  {
-    id: 8,
-    project_id: 3,
-    image_path: "/images/projects/project1/Image1.png",
-    is_cover: false,
-  },
-  {
-    id: 9,
-    project_id: 3,
-    image_path: "/images/projects/project1/Image2.png",
-    is_cover: false,
-  },
-  {
-    id: 10,
-    project_id: 4,
-    image_path: "/images/projects/project1/Image1.png",
-    is_cover: true,
-  },
-  {
-    id: 11,
-    project_id: 5,
-    image_path: "/images/projects/project1/Image2.png",
-    is_cover: true,
   },
 ];

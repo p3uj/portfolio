9b52a6ad532ab252b194936e86802b994d9f8ca6
BengieVillesco <bvillesco@gmail.com>
2026-05-17 13:29:55 +0800

    feat(sections/about): replace hardcoded about text with ABOUT_CONTENT description array mapping

--- components/sections/about/about-section.tsx
+++ components/sections/about/about-section.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./about-section.module.scss";
 import { Card } from "@/components/ui/card/card";
+import { ABOUT_CONTENT } from "@/data/mock/portfolio-content";
 import { useState } from "react";
 
 export default function AboutSection() {
@@ -24,15 +25,9 @@ export default function AboutSection() {
 
       <Card onClick={() => setOnClick(!onClick)}>
         <section className={styles.textWrapper}>
-          {/* prettier-ignore */}
-          <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente iusto illum corporis veniam vitae nobis beatae consequatur iure maiores sint perferendis voluptate accusamus minima numquam culpa quod, quos neque esse. <br /> <br />
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, mollitia hic? Commodi, est incidunt! Corporis commodi eum est ipsum. Accusamus minima sit incidunt? Sint architecto ipsam debitis alias impedit commodi. <br /> <br />
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos asperiores sunt quis doloribus eum nulla facere accusantium commodi eius, architecto illo quidem vero tempora magni alias sequi adipisci fuga cumque. <br /> <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, impedit eius repellendus expedita voluptatibus incidunt eaque quidem harum, beatae nihil dolore aliquid, porro nobis in aut sequi ipsam vitae magni? <br /> <br />
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique commodi quos aperiam quisquam ad debitis placeat nulla pariatur repellat voluptas perspiciatis, impedit amet odio, iusto ab. Aliquam quis totam praesentium. <br /> <br />
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos labore veritatis at id. Culpa iure rem ratione eaque earum, libero vel placeat obcaecati magnam necessitatibus sapiente possimus at consequatur quia?
-            </p>
+          {ABOUT_CONTENT.description.map((des, index) => (
+            <p key={index}>{des}</p>
+          ))}
         </section>
       </Card>
     </section>

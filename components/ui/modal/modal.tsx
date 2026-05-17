5cc5b7dd9b71c1a96f87328d56e98bd4928e4b05
BengieVillesco <bvillesco@gmail.com>
2026-05-17 23:40:00 +0800

    feat(ui/modal): add projectType prop to Modal and pass it to CardModal

--- components/ui/modal/modal.tsx
+++ components/ui/modal/modal.tsx
@@ -1,15 +1,17 @@
+import { ProjectType } from "@/types";
 import CardModal from "../card/card-modal";
 import styles from "./modal.module.scss";
 
 interface ModalProps {
   open: (value: boolean) => void;
   project_id: number | null;
+  project_type: ProjectType | null;
 }
 
-export default function Modal({ open, project_id }: ModalProps) {
+export default function Modal({ open, project_id, project_type }: ModalProps) {
   return (
     <div className={styles.modal} onClick={() => open(false)}>
-      <CardModal project_id={project_id} />
+      <CardModal project_id={project_id} project_type={project_type} />
     </div>
   );
 }

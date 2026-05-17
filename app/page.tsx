e10749310765f16b02eb23f3a7dfd4351697ccb3
BengieVillesco <bvillesco@gmail.com>
2026-05-17 23:38:35 +0800

    feat(page): add projectType state and pass it to modal and projects section

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,29 @@ import AboutSection from "../components/sections/about/about-section";
 import ProjectsSection from "../components/sections/projects/projects-section";
 import SkillsSections from "../components/sections/skills/skills-section";
 import ContactSection from "@/components/sections/contact/contack-section";
+import { ProjectType } from "@/types";
 
 export default function Page() {
   const [modalOpen, setModalOpen] = useState(false);
   const [projectId, setProjectId] = useState<number | null>(null);
+  const [projectType, setProjectType] = useState<ProjectType | null>(null);
 
   useEffect(() => {
     if (!modalOpen) {
       setProjectId(null);
+      setProjectType(null);
     }
   }, [modalOpen]);
 
   return (
     <>
-      {modalOpen && <Modal open={setModalOpen} project_id={projectId} />}
+      {modalOpen && (
+        <Modal
+          open={setModalOpen}
+          project_id={projectId}
+          project_type={projectType}
+        />
+      )}
 
       <ThemeToggle />
 
@@ -34,7 +43,11 @@ export default function Page() {
         <main>
           <HeroSection />
           <AboutSection />
-          <ProjectsSection modalOpen={setModalOpen} id={setProjectId} />
+          <ProjectsSection
+            modalOpen={setModalOpen}
+            id={setProjectId}
+            projectType={setProjectType}
+          />
           <SkillsSections />
           <ContactSection />
           <Footer />
